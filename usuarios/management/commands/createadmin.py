2f2f23318fb6b9bfaffecb2c79f929d65f666349
jesus <ssanchezc7@unemi.edu.ec>
2026-03-09 20:27:38 -0500

    fix admin auto creation

--- usuarios/management/commands/createadmin.py
+++ usuarios/management/commands/createadmin.py
@@ -1,11 +1,10 @@
 import os
-
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    help = "Create admin automatically"
+    help = "Create or update admin automatically"
 
     def handle(self, *args, **kwargs):
         username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
@@ -13,15 +12,20 @@ class Command(BaseCommand):
         password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
 
         if not username or not email or not password:
-            self.stdout.write(
-                self.style.WARNING(
-                    "Faltan variables DJANGO_SUPERUSER_USERNAME, DJANGO_SUPERUSER_EMAIL o DJANGO_SUPERUSER_PASSWORD"
-                )
-            )
+            self.stdout.write(self.style.WARNING("Faltan variables de entorno"))
             return
 
-        if not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(username, email, password)
+        user, created = User.objects.get_or_create(
+            username=username,
+            defaults={"email": email}
+        )
+
+        user.set_password(password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+
+        if created:
             self.stdout.write(self.style.SUCCESS("Admin creado"))
         else:
-            self.stdout.write("Admin ya existe")
+            self.stdout.write(self.style.SUCCESS("Admin actualizado"))
